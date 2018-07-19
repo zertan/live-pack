c3ea64716dda231d3c2b0cc20d31920f922388bb
Daniel Hermansson <daniel.hermansson@ericsson.com>
2018-07-19 16:14:30 +0200

    Initial commit

---
+++ config/bindings.el
@@ -0,0 +1,7 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+(windmove-default-keybindings 'super)
---
+++ init.el
@@ -0,0 +1,18 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)

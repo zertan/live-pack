9ac29e4c8f3c0b081d070c8ca00b6767ee02a10f
Daniel Hermansson <daniel@w3t.se>
2022-06-14 18:03:54 +0200

    Update keybindings

--- config/bindings.el
+++ config/bindings.el
@@ -4,10 +4,13 @@
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
 
-(windmove-default-keybindings 'super
-                              )
+(windmove-default-keybindings)
 
-(global-set-key (kbd "s-<left>")  'windmove-left)
-(global-set-key (kbd "s-<right>") 'windmove-right)
-(global-set-key (kbd "s-<up>")    'windmove-up)
-(global-set-key (kbd "s-<down>")  'windmove-down)
+(global-set-key (kbd "M-S-<left>")  'windmove-left)
+(global-set-key (kbd "M-S-<right>") 'windmove-right)
+(global-set-key (kbd "M-S-<up>")    'windmove-up)
+(global-set-key (kbd "M-S-<down>")  'windmove-down)
+
+
+;; magit
+(global-set-key (kbd "M-g") 'magit-status)
